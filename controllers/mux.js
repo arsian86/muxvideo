797151d7d98c636e292014fcca8961915d997620
Arsian Kuo <arsiankuo86@gmail.com>
2025-06-06 03:27:18 +0800

    測試webhook驗證並回傳playbackId成功

--- controllers/mux.js
+++ controllers/mux.js
@@ -1,8 +1,10 @@
 const { Mux } = require("@mux/mux-node");
 const config = require("../config/index");
 const { tokenId, tokenSecret, webhookSecret } = config.get("mux");
+const logger = require("../config/logger");
+const generateError = require("../utils/generateError");
 const AppDataSource = require("../db/data-source"); // 引入資料庫連線
-const videoRepo = AppDataSource.getRepository("Course_Video");
+const chapterRepo = AppDataSource.getRepository("Course_Chapter");
 const planRepo = AppDataSource.getRepository("Plan");
 
 const mux = new Mux({
@@ -14,88 +16,101 @@ const mux = new Mux({
 const { signPlayback } = require("../utils/mux");
 const { getLatestSubscription } = require("../services/checkServices");
 
-//教練上傳影片
+// 教練上傳影片
+// <mux-uploader>是 Mux 官方的 Web Component，底層用 upchunk 實作「分段上傳」。最大 200GB
 async function uploadVideo(req, res, next) {
   try {
     const chapterId = req.params.chapterId;
     // 從 Mux 取得一次性 upload URL
     const upload = await mux.video.uploads.create({
-      cors_origin: "http://localhost:3000",
+      cors_origin: "https://boston.zeabur.app",
       new_asset_settings: {
         playback_policy: ["signed"],
         video_quality: "plus",
       },
     });
     // 先存一筆暫時資料，記錄 uploadId 與 chapterId 的對應
-    await videoRepo.save({
-      chapter_id: chapterId,
+    await chapterRepo.save({
+      id: chapterId,
       mux_upload_id: upload.id,
       status: "uploading",
     });
     // 將 uploadUrl 與 uploadId 回傳給前端
     res.json({ uploadUrl: upload.url, uploadId: upload.id });
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    next(error);
   }
 }
 
 // webhook回傳影片信息，儲存資訊到資料庫
 async function videoUploadCheck(req, res, next) {
   try {
-    mux.webhooks.verifySignature(req.body.toString("utf8"), req.headers);
-    console.log(req.headers["mux-signature"]);
-  } catch (err) {
-    console.log(req.headers["mux-signature"]);
-    console.warn("webhook 驗簽失敗:", err.message);
-    return res.sendStatus(400);
+    // 驗證這個 webhook 確實來自 Mux
+    //req.body是物件格式，要轉成字串格式才能驗簽
+    mux.webhooks.verifySignature(JSON.stringify(req.body), req.headers, webhookSecret);
+    logger.info({ body: req.body, headers: req.headers }, "webhook 驗簽成功");
+  } catch (error) {
+    logger.warn({ body: req.body, headers: req.headers }, "webhook 驗簽失敗");
+    return next(generateError(400, "webhook 驗簽失敗"));
   }
-
-  const evt = JSON.parse(req.body); // 驗簽通過後再 parse
-  console.log("webhook received:", evt.type);
-
-  // 只關心轉檔完成的事件，其它類型可按需擴充
-  if (evt.type === "video.asset.ready") {
-    const assetId = evt.data.id;
-    const uploadId = evt.data.upload_id;
-    // 查出這個 uploadId 對應哪個 chapter
-    const video = await videoRepo.findOneBy({ mux_upload_id: uploadId });
-    if (!video) return res.status(404).json({ msg: "找不到對應章節" });
-    // 建立 signed playbackId
-    const playback = await mux.video.assets.createPlaybackId(assetId, {
-      policy: "signed",
-    });
-    //將assetId 與 playbackId存入course_video表
-    video.mux_asset_id = assetId;
-    video.mux_playback_id = playback.id;
-    video.status = "ready";
-    await videoRepo.save(video);
-
-    console.log("✔ asset.ready →", assetId, "→ playback", playback.id);
+  // 驗簽通過後，繼續處理 webhook 事件
+  try {
+    const evt = req.body; // 驗簽通過後再 parse
+    logger.info({ type: evt.type }, "webhook received:");
+    // 處理上傳完成的事件
+    if (evt.type === "video.asset.ready") {
+      console.log(evt);
+      const uploadId = evt.data.upload_id;
+      const assetId = evt.data.id;
+      // 查出這個 uploadId 對應哪個 chapter
+      const chapter = await chapterRepo.findOneBy({ mux_upload_id: uploadId });
+      if (!chapter) return next(generateError(404, "找不到對應章節"));
+      const signedPlayback = evt.data.playback_ids.find((playback) => playback.policy === "signed");
+      const signedPlaybackId = signedPlayback.id ? signedPlayback.id : null;
+      if (!signedPlaybackId) return next(generateError(404, "找不到對應播放 ID"));
+      chapter.mux_asset_id = assetId;
+      chapter.mux_playback_id = signedPlaybackId;
+      chapter.duration = evt.data.duration;
+      chapter.video_created_at = new Date(evt.data.created_at * 1000);
+      chapter.resolution_tier = evt.data.resolution_tier;
+      chapter.status = "ready";
+      await chapterRepo.save(chapter);
+      console.log("影片上傳完成，儲存到資料庫:", chapter);
+      res.status(200).json({
+        status: true,
+        message: "影片已成功上傳",
+        data: chapter,
+      });
+    } else {
+      logger.error({ error }, "影片尚未建立完成");
+      return next(generateError(400, "影片尚未建立完成"));
+    }
+  } catch (error) {
+    logger.error({ error }, "webhook 處理失敗:");
+    next(error);
   }
-  res.sendStatus(200);
 }
 
-// 學員請求播放
+// // 學員請求播放
 async function getVideoPlayer(req, res, next) {
-  const chapterId = req.params.chapterId;
-  const userId = req.user.id;
-  //判斷訂閱是否有效
-  const hasActiveSubscription = req.user.hasActiveSubscription;
-  if (!hasActiveSubscription) {
-    return next(generateError(403, "尚未訂閱或訂閱已失效，無可觀看課程類別"));
-  }
-  //取得此人最新的訂閱紀錄
-  const latestSubscription = await getLatestSubscription(userId);
-  const planId = latestSubscription.plan_id;
-  const plan = await planRepo.findOneBy({ id: planId });
-
-  const video = await videoRepo.findOneBy({ chapter_id: chapterId });
-  const playbackId = video.mux_playback_id;
-  if (!playbackId) return res.status(404).json({ msg: "no asset" });
-  const max = plan.max_resolution + "p";
-  const token = signPlayback(playbackId, max);
-  const streamUrl = `https://stream.mux.com/${playbackId}.m3u8?token=${token}`;
-  res.json({ streamUrl, title: video.title + " - " + video.subtitle });
+  // const chapterId = req.params.chapterId;
+  // const userId = req.user.id;
+  // //判斷訂閱是否有效
+  // const hasActiveSubscription = req.user.hasActiveSubscription;
+  // if (!hasActiveSubscription) {
+  //   return next(generateError(403, "尚未訂閱或訂閱已失效，無可觀看課程類別"));
+  // }
+  // //取得此人最新的訂閱紀錄
+  // const latestSubscription = await getLatestSubscription(userId);
+  // const planId = latestSubscription.plan_id;
+  // const plan = await planRepo.findOneBy({ id: planId });
+  // const video = await videoRepo.findOneBy({ chapter_id: chapterId });
+  // const playbackId = video.mux_playback_id;
+  // if (!playbackId) return res.status(404).json({ msg: "no asset" });
+  // const max = plan.max_resolution + "p";
+  // const token = signPlayback(playbackId, max);
+  // const streamUrl = `https://stream.mux.com/${playbackId}.m3u8?token=${token}`;
+  // res.json({ streamUrl, title: video.title + " - " + video.subtitle });
 }
 
 module.exports = {
